c9a45cf8394af3f8b92eec2cb448dec0ff0ed659
Sagnik Ganguly <sagnikg2013@gmail.com>
2020-05-16 15:33:29 +0530

    chatbox started

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo, { ReactComponent } from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          no
-        </a>
-      </header>
-    </div>
-  );
+
+  class Messages extends React.Component{
+    constructor(props){
+      super(props);
+      this.state = {
+        messages : this.props.messages
+      }
+    }
+    render(){
+      var msgz = this.state.messages.map( (msg) => <li>{msg}</li>);
+      return (
+          <ul id="messages">
+            {msgz}
+          </ul>
+      );
+    }
+  }
+  
+  var servedMessages = [];
+  function SendMessage(str){
+      servedMessages.push(str);
+      ReactDOM.render(
+        // <React.StrictMode>
+        <Messages messages={servedMessages}/>,
+        // </React.StrictMode>,
+        document.getElementById('messageHolder')
+      );
+  }
+
+  class Chat extends React.Component {
+    constructor(props){
+      super(props);
+      this.handleChange = this.handleChange.bind(this);
+      this.keyPress = this.keyPress.bind(this);
+      this.state = {
+        name : this.props.name,
+        currentmsg : ''
+      }
+    }
+
+    keyPress(e){
+      if(e.keyCode == 13)
+       {
+        SendMessage(this.state.name + ": "+ this.state.currentmsg);
+        e.target.value = '';
+       }
+    }
+
+    handleChange(e) {
+      this.state.currentmsg = e.target.value;
+    }
+
+    render(){
+     // var msgz = this.state.messages.map( (msg) => <li>{msg}</li>);
+      return (
+        <div id="chat">
+          <div id="messageHolder">
+            <Messages messages={servedMessages}/>
+          </div>
+          <div id="typehold">
+            <input type="text" onChange={this.handleChange} onKeyDown={this.keyPress} placeholder="enter a message..."></input>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  return <Chat name="haha"/>
 }
 
 export default App;
+
+
